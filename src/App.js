6fc9c3bb3595f1552d3f8e572ab782776ff8e0a8
Daniel Zawlocki <d.zawlocki@eltrox.pl>
2017-11-22 14:55:41 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,70 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import firebase from 'firebase'
-import { Header } from './components/common'
+import { Header, Button, Spinner, Card, CardSection } from './components/common'
 import LoginForm from './components/LoginForm'
 
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            loggedIn: null,
+        }
+
+        this.renderContent = this.renderContent.bind(this)
+    }
+
     componentWillMount() {
         firebase.initializeApp({
-            apiKey: 'AIzaSyDvRrGRkc52ILI_NYxzmnAIqv39lRsObyE',
-            authDomain: 'auth-test-835e7.firebaseapp.com',
-            databaseURL: 'https://auth-test-835e7.firebaseio.com',
-            projectId: 'auth-test-835e7',
-            storageBucket: 'auth-test-835e7.appspot.com',
-            messagingSenderId: '365852175863',
+            apiKey: 'AIzaSyBWPhppNwNG5qK2ywXyTo4aiVFkvG1Nuks',
+            authDomain: 'authreact-cecd6.firebaseapp.com',
+            databaseURL: 'https://authreact-cecd6.firebaseio.com',
+            projectId: 'authreact-cecd6',
+            storageBucket: 'authreact-cecd6.appspot.com',
+            messagingSenderId: '1020323890161',
         })
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true })
+            } else {
+                this.setState({ loggedIn: false })
+            }
+        })
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case false:
+                return <LoginForm />
+
+            case true:
+                return (
+                    <Card>
+                        <CardSection>
+                            <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+                        </CardSection>
+                    </Card>
+                )
+
+            default:
+                return (
+                    <Card>
+                        <CardSection>
+                            <Spinner size="large" />
+                        </CardSection>
+                    </Card>
+                )
+        }
     }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         )
     }
